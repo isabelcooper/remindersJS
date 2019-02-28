1d42f4960854a79105f581a7bec9992bcd059a8a
isabelcooper <isabel@triptease.com>
2019-02-28 10:01:45 +0000

    add express and beginning of connection setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Notes from './components/notes';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p> In app.js </p>
-
+          <Notes />
         </header>
       </div>
     );
